91c778fce081ee2f02b47b616fa169e240cfe21d
unknown <aashishmaharjan700@gmail.com>
2023-05-22 10:04:24 +0545

    last commit

---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+  secret:
+    'DO NOT USbhjgu5228898THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+  refreshSecret: 'hello',
+};
